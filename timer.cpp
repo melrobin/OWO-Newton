25a012227747d39a20c16be780b308af32fdd064
melrobin <melrobin@gmail.com>
2015-04-27 01:18:32 -0500

    Add timer.cpp file

---
+++ timer.cpp
@@ -0,0 +1,61 @@
+/*
+ * * This program calculates the time required to
+ * * execute the program specified as its first argument.
+ * * The time is printed in seconds, on standard out.
+ * */
+#include <cstdio>
+#include <unistd.h>
+#include <cstdlib>
+#include <ctime>
+
+#define BILLION  1000000000L;
+double tic()
+{
+    struct timespec start;
+    double accum;
+
+    if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
+      perror( "clock gettime" );
+      return EXIT_FAILURE;
+    }
+
+    accum =   start.tv_sec + (double) start.tv_nsec/(double)BILLION;
+    return accum;
+}
+double toc()
+{
+   struct timespec stop;
+   double accum;
+
+   if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) 
+    {
+      perror( "clock gettime" );
+      return EXIT_FAILURE;
+    }
+    accum =   stop.tv_sec + (double) stop.tv_nsec/(double)BILLION;
+    return accum;
+
+}
+/*int main( int argc, char** argv )
+  {
+    struct timespec start, stop;
+    double accum;
+    int i;
+
+    if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
+      perror( "clock gettime" );
+      return EXIT_FAILURE;
+    }
+
+    for (i=0;i<1000000;i++);
+    if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) {
+      perror( "clock gettime" );
+      return EXIT_FAILURE;
+    }
+
+    accum = ( stop.tv_sec - start.tv_sec )
+            + (double)( stop.tv_nsec - start.tv_nsec )
+              / (double)BILLION;
+    printf( "%lf\n", accum );
+    return EXIT_SUCCESS;
+  } */
